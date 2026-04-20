3d67f0d8bfffdf7f5e9852a3c48c247d9d7e02b7
한수민 <132890787+2024125085@users.noreply.github.com>
2026-04-20 15:57:46 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -5,18 +5,26 @@
 int main(void)
 {
     // 변수선언 및 초기화
-    int i,num=0,guess=0,n=0;
+    int i,num=0,guess=0,n=1;
     scanf("%d",&num);
+    
     scanf("%d",&guess);
     // 입력받은 두 값이 같지 않을 때마다 반복
     do{
-        if(num > guess) printf("%d<?",guess);
-        else printf("%d>?",guess);
+        if(num == guess){
+            break;
+        }
+        else if(num > guess){
+            printf("%d<?\n",guess);
+        }
+        else {
+            printf("%d>?\n",guess);
+        }
         scanf("%d",&guess);
         n++;
     }while(num != guess);
     // n회 반복할 때마다 증가시킨 변수 n으로 시도횟수 계산
-    printf("%d==?",guess);
+    printf("%d==?\n",guess);
     printf("%d",n);
     
     return 0;
